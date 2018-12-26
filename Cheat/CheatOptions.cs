974b1dda5fd3fd50fb66c53c27bca7ed69a46037
b74k <45390128+b74k@users.noreply.github.com>
2018-12-26 05:58:05 +0100

    Update CheatOptions.cs

--- Cheat/CheatOptions.cs
+++ Cheat/CheatOptions.cs
@@ -8,33 +8,24 @@ namespace Shelter.CheatOpt
     {
         private void OnGUI()
         {
+            // Need to fix, since it redraws on every single frame.
             if (Options.Watermark)
-            {
-                GUI.Label(new Rect(20, 20, 200, 60), "UnKnoWnCheaTs.me | Shv00");
-            }
+                GUI.Label(new Rect(20, 20, 200, 60), "UnKnoWnCheaTs.me | B74k");
         }
 
         private void Update()
         {
             if (Input.GetKey(KeyCode.Delete))
-            {
                 Loader.Unload();
-            }
 
             if(Input.GetKey(KeyCode.F7))
-            {
                 VaultOptions.InfiniteEnergy(5000);
-            }
 
             if (Input.GetKey(KeyCode.F8))
-            {
                 VaultOptions.InfiniteFood(5000);
-            }
 
             if (Input.GetKey(KeyCode.F9))
-            {
                 VaultOptions.InfiniteWater(5000);
-            }
         }
     }
 }
